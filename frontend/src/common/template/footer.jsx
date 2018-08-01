2a708cabb4f012a525d4546f4e8446160b8b97ae
Breno Oliveira dos Santos <breno.oliver07@gmail.com>
2018-08-01 15:25:40 -0300

    templates e rotas

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default props => (
+    <footer className='main-footer'>
+        <strong>
+            Copyright &copy; 2018
+            <a href="http://github.com/brenoos" target='_blank'> Breno Oliveira</a>.
+        </strong>
+    </footer>
+)
