b1c5d4298b84d5cae220ffc1cfc402eaf9e038d2
Breno Oliveira dos Santos <breno.oliver07@gmail.com>
2018-08-15 09:57:28 -0300

    tab e IF

--- frontend/src/common/tab/TabContent.jsx
+++ frontend/src/common/tab/TabContent.jsx
@@ -2,15 +2,20 @@ import React, { Component } from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 
+import If from '../operator/if'
+
 class TabContent extends Component {
     render(){
         const {id, children, tab} = this.props
         let selected = tab.selected === id
+        let visible = tab.visible[id]
         return(
-            <div id={id}
-                className={`tab-pane ${selected ?  'active' : ''}`}>
-                {children}
-            </div>
+            <If test={visible}>
+                <div id={id}
+                    className={`tab-pane ${selected ?  'active' : ''}`}>
+                    {children}
+                </div>
+            </If>
         )
     }
 }
--- frontend/src/common/tab/tabHeader.jsx
+++ frontend/src/common/tab/tabHeader.jsx
@@ -2,22 +2,25 @@ import React, { Component } from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 
+import If from '../operator/if'
 import { selectTab } from './tabActions'
 
 class TabHeader extends Component {
     render(){
         const { selectTab, target, icon, label, tab } = this.props
         let selected = tab.selected === target
-        
+        let visible = tab.visible[target]
         return(
-            <li className={selected ? 'active' : ''}>
-                <a href='javascript:;'
-                    data-toggle='tab'
-                    onClick={() => selectTab(target)}
-                    data-target={target}>
-                    <i className={`fa fa-${icon}`}></i> {label}
-                </a>
-            </li>
+            <If test={visible}>
+                <li className={selected ? 'active' : ''}>
+                    <a href='javascript:;'
+                        data-toggle='tab'
+                        onClick={() => selectTab(target)}
+                        data-target={target}>
+                        <i className={`fa fa-${icon}`}></i> {label}
+                    </a>
+                </li>
+            </If>
         )
     }
 }
