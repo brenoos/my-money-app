8589c997c82906ce000b610698267ac726d5bb0d
Breno Oliveira dos Santos <breno.oliver07@gmail.com>
2018-08-16 11:41:06 -0300

    cycle list complete

--- frontend/src/billingCycle/billingCycleList.jsx
+++ frontend/src/billingCycle/billingCycleList.jsx
@@ -10,6 +10,17 @@ class BillingCycleList extends Component {
         this.props.getList()
     }
     
+    renderRows(){
+        const list = this.props.list || []
+        return list.map(bc => (
+            <tr key={bc._id}>
+                <td>{bc.name}</td>
+                <td>{bc.month}</td>
+                <td>{bc.year}</td>
+            </tr>
+        ))
+    }
+
     render(){
         return(
             <div>
@@ -20,10 +31,10 @@ class BillingCycleList extends Component {
                             <th>Mês</th>
                             <th>Ano</th>
                         </tr>
-                        <tbody>
-
-                        </tbody>
                     </thead>
+                        <tbody>
+                            {this.renderRows()}
+                        </tbody>
                 </table>
             </div>
         )
